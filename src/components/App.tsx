fa657ebeabb96a4419902da23217421f15cd471b
nemutas <inohiro.focru@gmail.com>
2022-03-17 01:52:30 +0900

    first commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,10 @@ export const App: VFC = () => {
 	return (
 		<div style={{ width: '100vw', height: '100vh' }}>
 			<TCanvas />
-			<LinkIconButton imagePath="/assets/icons/github.svg" linkPath="https://github.com/nemutas/r3f-gyroid-sphere" />
+			<LinkIconButton
+				imagePath="/assets/icons/github.svg"
+				linkPath="https://github.com/nemutas/r3f-gyroid-sphere-operable"
+			/>
 		</div>
 	)
 }
